c28e2492ab2d9f2bf791e4067ce52745488d9033
mooralec <mooralec@iu.edu>
2023-07-17 16:17:50 -0400

    Final unit-2-1 Commit

--- app.py
+++ app.py
@@ -1,82 +1,61 @@
-# Copyright © 2023, Indiana University
-# BSD 3-Clause License
-
-import csv 
-
+import csv
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
-EMPLOYEES_PATH = app.root_path + '/employees.csv'
-CUSTOMERS_PATH = app.root_path + '/customers.csv'
-LAWNS_PATH = app.root_path + '/lawns.csv'
+EMPLOYEES_PATH = "employees.csv"
+CUSTOMERS_PATH = "customers.csv"
+LAWNS_PATH = "lawns.csv"
 
-def read_csv_file(file_path):
-    """Reads data from a CSV file and returns a list of dictionaries."""
-    data = []
-    with open(file_path, "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-    return data
-
-@app.route("/")
+@app.route('/')
 def index():
+    """Renders the index page with basic information and navigation buttons."""
     return render_template("index.html")
 
 @app.route('/employees/')
 def employees():
-    # Read the employee data from the CSV file
-    with open('employees.csv', 'r') as file:
+    """Renders the employees page with a list of employees sorted by start date."""
+    with open(EMPLOYEES_PATH, 'r') as file:
         reader = csv.DictReader(file)
-        employees = list(reader)
-
-    # Sort the employees by start_date in descending order
-    employees = sorted(employees, key=lambda e: e['start_date'], reverse=True)
-
-    return render_template('employees.html', employees=employees)
+        employees = sorted(list(reader), key=lambda x: x['employment_start_date'], reverse=True)
+    return render_template("employees.html", employees=employees)
 
 @app.route('/employees/<employee_id>/')
 def employee(employee_id):
-    # Read the employee data from the CSV file
-    with open('employees.csv', 'r') as file:
+    """Renders the employee details page for the given employee ID."""
+    with open(EMPLOYEES_PATH, 'r') as file:
         reader = csv.DictReader(file)
-        employees = list(reader)
-
-    # Find the employee with the matching employee_id
-    employee = next((e for e in employees if e['employee_id'] == employee_id), None)
-
-    return render_template('employee.html', employee=employee)
+        employee = next((emp for emp in reader if emp['employee_id'] == employee_id), None)
+    return render_template("employee.html", employee=employee)
 
 @app.route('/customers/')
 def customers():
-    # Read customer data from CSV file and store it in a list of dictionaries
-    with open('customers.csv', 'r') as file:
-        reader = csv.DictReader(file)
-
-    customers = sorted(reader, key=lambda c: c['last_name'])
-
+    """Reads customer data from the 'customers.csv' file, sorts it by last name from A to Z, and returns a rendered template."""
+    try:
+        with open(CUSTOMERS_PATH, 'r') as file:
+            reader = csv.DictReader(file)
+            customers = sorted(reader, key=lambda row: row['name'].split()[-1])
+    except Exception as e:
+        print(e)
+        customers = []
+    
     return render_template('customers.html', customers=customers)
 
 @app.route('/lawns/')
 def lawns():
-    # Read lawn data from CSV file and store it in a list of dictionaries
-    with open('lawns.csv', 'r') as file:
+    """Renders the lawns page with a list of lawns sorted by size."""
+    with open(LAWNS_PATH, 'r') as file:
         reader = csv.DictReader(file)
+        lawns = sorted(list(reader), key=lambda x: int(x['size']), reverse=True)
+    return render_template("lawns.html", lawns=lawns)
 
-    lawns = sorted(reader, key=lambda l: int(l['size']), reverse=True)
-
-    return render_template('lawns.html', lawns=lawns)
-
-@app.route('/lawns/<int:lawn_id>/')
+@app.route('/lawns/<lawn_id>/')
 def lawn(lawn_id):
-    # Read the specific lawn data from the CSV file based on the lawn_id
-    with open('lawns.csv', 'r') as file:
+    """Renders the lawn details page for the given lawn ID."""
+    with open(LAWNS_PATH, 'r') as file:
         reader = csv.DictReader(file)
-        lawn = next((l for l in reader if int(l['id']) == lawn_id), None)
-
-    return render_template('lawn.html', lawn=lawn)
+        lawn = next((l for l in reader if l['id'] == lawn_id), None)
+    return render_template("lawn.html", lawn=lawn)
 
 if __name__ == "__main__":
     app.run(debug=True)
