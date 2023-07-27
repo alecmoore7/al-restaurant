245aefb3d3d6f16b265cf0a99c1f99d0069ad70a
mooralec <mooralec@iu.edu>
2023-07-27 03:18:38 -0400

    Server Check

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pymysql
 from flask import Flask, render_template, redirect, url_for, request
 import csv  # Used for reading and writing event data via csv.
 from os.path import exists 
@@ -17,139 +18,153 @@ LAWN_KEYS = ['address','size','date_added','notes']
 CUSTOMER_PATH = app.root_path + '/customers.csv'
 CUSTOMER_KEYS = ['name','address','email','dob','phone']
 EMPLOYEE_PATH = app.root_path + '/employees.csv'
-EMPLOYEE_KEYS = ['name','address','email','dob','phone','start_date','title']
-
-def get_lawns():
-    #create new list to hold the dictionaries
-    return database.get_lawns()
-
-def set_lawns(lawns):
-    database.set_lawns(lawns)
-
-def get_customers():
-    #create new list to hold the dictionaries
-    return database.get_customers()
-
-def set_customers(customers):
-    database.set_customers(customers)
-
-def get_employees():
-    #create new list to hold the dictionaries
-    return database.get_employees()
-
-def set_employees(employees):
-    database.set_employees(employees)
+EMPLOYEE_KEYS = ['name','address','title','email','dob','phone','start_date']
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def load_customers():
+    return database.get_customers()
+
+def load_lawns():
+    return database.get_lawns()
+
+def load_employees():
+    return database.get_employees()
+
 @app.route('/lawns/')
 def list_lawns():
-    lawns = get_lawns()
+    lawns = load_lawns()
     lawns= sorted(lawns, key=lambda e: e['size'], reverse=True)
     return render_template('lawns.html', lawns=lawns)
 
 @app.route('/employees/')
 def list_employees():
-    employees = get_employees()
+    employees = load_employees()
     employees= sorted(employees, key=lambda e: e['start_date'])
     return render_template('employees.html', employees=employees)
 
 @app.route('/customers/')
 def list_customers():
-    customers = get_customers()
+    customers = load_customers()
     customers=sorted(customers, key=lambda e: e['name'])
     return render_template('customers.html', customers=customers)
 
-@app.route('/employees/<employee_id>/')
-def view_employee(employee_id=None):
-    if employee_id:
-        #grab employee_id from route and convert to int so we can use it as an index
-        employee_id = int(employee_id)
-        employees = get_employees()
 
-        return render_template('employee.html', employee_id=employee_id, employee=employees[employee_id])
+@app.route('/employees/<employee_id>/')
+def view_employee(employee_id):
+    employee = database.get_employee(employee_id)
+    if employee:
+        return render_template('employee.html', employee_id=employee_id, employee=employee)
     else:
         return redirect(url_for('list_employees'))
 
+
 @app.route('/lawns/<lawn_id>/')
-def view_lawn(lawn_id=None):
-    if lawn_id:
-        #grab lawn_id from route and convert to int so we can use it as an index
-        lawn_id = int(lawn_id)
-        lawns = get_lawns()
-        return render_template('lawn.html', lawn_id=lawn_id, lawn=lawns[lawn_id])
+def view_lawn(lawn_id):
+    lawn = database.get_lawn(lawn_id)
+    if lawn:
+        return render_template('lawn.html', lawn_id=lawn_id, lawn=lawn)
     else:
         return redirect(url_for('list_lawns'))
 
+@app.route('/employee/create', methods=['GET', 'POST'])
+def create_employee():
+    if request.method == 'POST':
+        employee = {
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'title': request.form['title'],
+            'address': request.form['address'],
+            'email': request.form['email'],
+            'dob': request.form['dob'],
+            'phone': request.form['phone'],
+            'start_date': request.form['start_date'],
+        }
+
+        database.add_employee(employee)
+
+        return redirect(url_for('list_employees'))
+    
+    return render_template('employee_form.html', employee=None)
+    
 @app.route('/lawns/create', methods=['GET', 'POST'])
 def create_lawn():
     #if we have form data coming in via POST
     if request.method == 'POST':
-        #grab the full lawns data and make a new empty dictionary for the incoming data
-        lawns = get_lawns()
-        lawn = {}
-        # grab the form data coming in and store in the new dict
-        lawn['name'] = request.form['name']
-        lawn['pet_type'] = request.form['pet_type']
-        lawn['level'] = request.form['level']
-        lawn['start_date'] = request.form['start_date']
-        lawn['start_time'] = request.form['start_time']
-        lawn['duration'] = request.form['duration']
-        lawn['length'] = request.form['length']
-        lawn['trainer'] = request.form['trainer']
-        lawn['desc'] = request.form['desc']
-        #add the new dict to our lawns data
-        lawns.append(lawn)
-        
-        # Make sure events are sorted by date.
-        lawns = sorted(lawns, key=lambda e: e['start_date'])
+        lawn = {
+            'address' : request.form['address'],
+            'size' : request.form['size'],
+            'lawn_type' : request.form['lawn_type'],
+            'date_added' : request.form['date_added'],
+            'notes' : request.form['notes'],
+        }
+        database.add_lawn(lawn)
 
-        # Write data back out to csv.
-        set_lawns(lawns)
-
-        # Return to the list of events.
         return redirect(url_for('list_lawns'))
-    else:
-        return render_template('lawn_form.html')
+
+    return render_template('lawn_form.html')
+
+@app.route('/employees/<employee_id>/edit', methods=['GET', 'POST'])
+def edit_employee(employee_id=None):
+    employee = database.get_employee(employee_id)
+    if employee:
+        if request.method== 'POST':
+            employees = load_employees()
+            employee_id = int(employee_id)
+            employees[employee_id]['first_name']: request.form['first_name']
+            employees[employee_id]['last_name']: request.form['last_name']
+            employees[employee_id]['title']: request.form['title']
+            employees[employee_id]['address']: request.form['address']
+            employees[employee_id]['email']: request.form['email']
+            employees[employee_id]['dob']: request.form['dob']
+            employees[employee_id]['phone']: request.form['phone']
+            employees[employee_id]['start_date']: request.form['start_date']
+            employees.append(employee)
+        
+            database.update_employee(employee_id, employee)
+
+            return redirect(url_for('list_employee', employee_id=employee_id)) 
+        else:
+            employee_id = int(employee_id)
+            employees = load_employees
+            employee = employees[employee_id]
+            return render_template('employee_form.html', employee_id=employee_id, employee=employee)
+    
+        
 
 @app.route('/lawns/<lawn_id>/edit', methods=['GET', 'POST'])
 def edit_lawn(lawn_id=None):
-    #if we have form data coming in via POST
-    if request.method== 'POST':
-        #grab the full lawns data
-       lawns = get_lawns() 
-       lawn_id=int(lawn_id)
-        # grab the form data coming in and replace values in lawns data
-       lawns[lawn_id]['name'] = request.form['name']
-       lawns[lawn_id]['pet_type'] = request.form['pet_type']
-       lawns[lawn_id]['level'] = request.form['level']
-       lawns[lawn_id]['start_date'] = request.form['start_date']
-       lawns[lawn_id]['start_time'] = request.form['start_time']
-       lawns[lawn_id]['duration'] = request.form['duration']
-       lawns[lawn_id]['length'] = request.form['length']
-       lawns[lawn_id]['desc'] = request.form['desc']
-       lawns[lawn_id]['trainer'] = request.form['trainer']
-       #save the modified lawns list of dictionaries
-       set_lawns(lawns)
-       #redirect to the lawn page 
-       return redirect(url_for('view_lawn', lawn_id=lawn_id)) 
-    else:
-        #no form data, show the basic edit form with lawn data passed in
-        lawn_id = int(lawn_id)
-        lawns = get_lawns()
-        lawn=lawns[lawn_id]
-        return render_template('lawn_form.html', lawn_id=lawn_id, lawn=lawn)
+    lawn = database.get_lawn(lawn_id)
+    if lawn:
+        if request.method== 'POST':
+            lawns = load_lawns()
+            lawn_id = int(lawn_id)
+            lawns[lawn_id]['address']: request.form['address']
+            lawns[lawn_id]['size']: request.form['size']
+            lawns[lawn_id]['lawn_type']: request.form['lawn_type']
+            lawns[lawn_id]['date_added']: request.form['date_added']
+            lawns[lawn_id]['notes']: request.form['notes']
+            lawns.append(lawn)
+        
+            database.update_lawn(lawn_id, employee)
+
+            return redirect(url_for('list_lawn', lawn_id=lawn_id)) 
+        else:
+            lawn_id = int(lawn_id)
+            lawns = load_lawns
+            employee = lawns[lawn_id]
+            return render_template('lawn_form.html', lawn_id=lawn_id, lawn=lawn)
         
 @app.route('/lawn/<lawn_id>/delete', methods=['GET', "POST"])
 def delete_lawn(lawn_id=None):
     lawn_id = int(lawn_id)
     delete=request.args.get('delete', None)
-    lawns = get_lawns()
+    lawns = load_lawns()
     if delete == "1" and lawn_id < len(lawns):
         del lawns[lawn_id]
-        set_lawns(lawns)
+        database.update_lawn(lawns)
         return redirect(url_for('list_lawns'))  
     else:
         lawn=lawns[lawn_id]
--- database.py
+++ database.py
@@ -1,132 +1,199 @@
 import pymysql
-
 # Uncomment the following line when you start project 3.2:
 from app import app
 
 def get_connection():
-    return pymysql.connect(
-        host="db.luddy.indiana.edu",
-        user="your_username",
-        password="your_password",
-        database="Your_DB_password",
-        cursorclass=pymysql.cursors.DictCursor
-    )
+    return pymysql.connect(host=app.config['DB_HOST'],
+                           user=app.config['DB_USER'],
+                           password=app.config['DB_PASS'],
+                           database=app.config['DB_DATABASE'],
+                           cursorclass=pymysql.cursors.DictCursor)
 
 def get_lawns():
     '''Returns a list of dictionaries representing all of the lawns data'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM lawns")
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM lawns")
             lawns = cursor.fetchall()
-    return lawns
+        return lawns
+    except Exception as e:
+        print("Error: ", e)
+        return []
+
 
 def get_lawn(lawn_id):
     '''Takes a lawn_id, returns a single dictionary containing the data for the lawn with that id'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM lawns WHERE id = %s", (lawn_id,))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM lawns WHERE id = %s", (lawn_id,))
             lawn = cursor.fetchone()
+    except Exception as e:
+        print("Error: ", e)
+        lawn = None
+    finally:
+        connection.close()
     return lawn
 
 def add_lawn(lawn):
     '''Takes as input all of the data for a lawn. Inserts a new lawn into the lawns table'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO lawns (address, size, date_added, notes) VALUES (%s, %s, %s, %s)"
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO lawns (address, size, date_added, notes) VALUES (%s, %s, %s, %s)"
             cursor.execute(sql, (lawn['address'], lawn['size'], lawn['date_added'], lawn['notes']))
         connection.commit()
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
+
 
 def update_lawn(lawn_id, lawn):
     '''Takes a lawn_id and data for a lawn. Updates the lawns table with new data for the lawn with lawn_id as its primary key'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "UPDATE lawns SET address=%s, size=%s, date_added=%s, notes=%s WHERE id=%s"
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "UPDATE lawns SET address=%s, size=%s, date_added=%s, notes=%s WHERE id=%s"
             cursor.execute(sql, (lawn['address'], lawn['size'], lawn['date_added'], lawn['notes'], lawn_id))
         connection.commit()
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
 
 def lawn_owner(customer_id, lawn_id):
     '''Adds an entry into the database that the lawn with primary key lawn_id is owned/managed by the customer with customer_id'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO lawn_owners (customer_id, lawn_id) VALUES (%s, %s)"
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO lawn_owners (customer_id, lawn_id) VALUES (%s, %s)"
             cursor.execute(sql, (customer_id, lawn_id))
         connection.commit()
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
 
 def get_lawns_customers(customer_id):
     '''Returns a list of dictionaries representing all of the lawns owned/managed by the customer with primary key of customer_id'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "SELECT l.* FROM lawns l INNER JOIN lawn_owners lo ON l.id=lo.lawn_id WHERE lo.customer_id = %s"
-            cursor.execute(sql, (customer_id,))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT l.* FROM lawns l INNER JOIN lawn_owners lo ON l.id=lo.lawn_id WHERE lo.customer_id = %s", (customer_id))
             lawns = cursor.fetchall()
-    return lawns
+        return lawns
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
 
 def get_owner(lawn_id):
     '''Returns a dictionary with the data for the owner of the lawn with primary key lawn_id'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "SELECT c.* FROM customers c INNER JOIN lawn_owners lo ON c.id=lo.customer_id WHERE lo.lawn_id = %s"
-            cursor.execute(sql, (lawn_id,))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT c.* FROM customers c INNER JOIN lawn_owners lo ON c.id=lo.customer_id WHERE lo.lawn_id = %s", (lawn_id))
             owner = cursor.fetchone()
+    except Exception as e:
+        print ("error: ", e)
+        owner = None
+    finally:
+        connection.close
     return owner
+    
 
 def get_customers():
     '''Returns a list of dictionaries representing all of the customer data'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM customers")
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM customers")
             customers = cursor.fetchall()
-    return customers
+        return customers
+    except Exception as e:
+        print("Error:", e)
+        return []
 
 def add_customer(customer):
     '''Takes all of the data required for a customer and inserts it into the customers table'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO customers (first_name, last_name, address, email, phone, dob, start_date, title) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-            cursor.execute(sql, (
-                customer['first_name'], customer['last_name'], customer['address'], customer['email'],
-                customer['phone'], customer['dob'], customer['start_date'], customer['title']
-            ))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO customers (name, address, email, dob, phone) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (customer['name'], customer['address'], customer['email'], customer['dob'],customer['phone']))
         connection.commit()
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
 
 def get_employees():
     '''Returns a list of dictionaries representing all of the employees data'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM employees")
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM employees")
             employees = cursor.fetchall()
-    return employees
+        return employees
+    except Exception as e:
+        print("Error: ", e)
+        return []
+
 
 def get_employee(employee_id):
     '''Takes an employee_id, returns a single dictionary containing the data for the employee with that id'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM employees WHERE id = %s", (employee_id,))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM employees WHERE id = %s", (employee_id,))
             employee = cursor.fetchone()
+    except Exception as e:
+        print("Error: ", e)
+        employee = None
+    finally:
+        connection.close()
     return employee
 
 def add_employee(employee):
     '''Takes as input all of the data for an employee. Inserts a new employee into the employees table'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO employees (first_name, last_name, address, email, phone, dob, start_date, title) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-            cursor.execute(sql, (
-                employee['first_name'], employee['last_name'], employee['address'], employee['email'],
-                employee['phone'], employee['dob'], employee['start_date'], employee['title']
-            ))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO employees (first_name, last_name, title, address, email, phone, dob, start_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (employee['first_name'], employee['last_name'], employee['title'], employee['address'], employee['email'],employee['phone'], employee['dob'], employee['start_date']))
         connection.commit()
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
 
 def update_employee(employee_id, employee):
     '''Takes an employee_id and data for an employee. Updates the employees table with new data for the employee with employee_id as its primary key'''
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            sql = "UPDATE employees SET first_name=%s, last_name=%s, address=%s, email=%s, phone=%s, dob=%s, start_date=%s, title=%s WHERE id=%s"
-            cursor.execute(sql, (
-                employee['first_name'], employee['last_name'], employee['address'], employee['email'],
-                employee['phone'], employee['dob'], employee['start_date'], employee['title'], employee_id
-            ))
+    try:
+        connection = get_connection()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "UPDATE employees SET first_name=%s, last_name=%s, title=%s, address=%s, email=%s, phone=%s, dob=%s, start_date=%s WHERE id=%s"
+            cursor.execute(sql, (employee['first_name'], employee['last_name'], employee['title'], employee['address'], employee['email'],employee['phone'], employee['dob'], employee['start_date'], employee_id))
         connection.commit()
-
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        connection.close()
 if __name__ == '__main__':
     # add more test code here to make sure all your functions are working correctly
     try:
